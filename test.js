cdbba64b27ae146556a565b676305699cd58e83b
shri7756 <shri.hallikhed@gmail.com>
2015-09-22 14:31:47 +0530

    Update test.js

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ describe('User visits Hello World page', function() {
     });
 
     it('should see Hello World', function() {
-      browser.assert.text('h2', 'Wel come to Mindtree');
+      browser.assert.text('h2', 'Wel come to Mindtree Mr.');
     });
   });
 });
